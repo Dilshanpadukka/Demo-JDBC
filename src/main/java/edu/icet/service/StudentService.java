c9d4499404211d03a1e989413320a982d3410081
Dilshanpadukka <udayankadilshan23@gmail.com>
2024-08-23 16:50:01 +0530

    first commit

---
+++ src/main/java/edu/icet/service/StudentService.java
@@ -0,0 +1,20 @@
+package edu.icet.service;
+
+
+import edu.icet.dto.Student;
+
+import java.util.List;
+
+public interface StudentService{
+    List<Student> getStudent();
+
+    void addStudent(Student student);
+
+    void deleteStudentById(Integer id);
+
+    void updateStudent(Student student);
+
+    List<Student> findByName(String name);
+
+    void addStudents(List<Student> students);
+}
